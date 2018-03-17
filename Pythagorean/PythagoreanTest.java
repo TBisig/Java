1e55ed96007b2cefb35c394eb1cc10dfc600aeba
TBisig <34490301+TBisig@users.noreply.github.com>
2018-03-16 17:38:48 -0700

    first commit

---
+++ Pythagorean/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean p = new Pythagorean();
+        Double hypotenuse = p.calculateHypotenuse(9,12);
+        System.out.println(hypotenuse);
+    }
+}
