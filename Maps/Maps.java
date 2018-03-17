fe2a388b36b995877612987d68dab4119068100c
TBisig <34490301+TBisig@users.noreply.github.com>
2018-03-16 17:37:41 -0700

    Add files via upload

---
+++ Maps/Maps.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+
+public class Maps {
+    public HashMap<String, String> trackList(int[] arr) {
+        HashMap<String, String> userMap = new HashMap<String, String>();
+    }
+}
